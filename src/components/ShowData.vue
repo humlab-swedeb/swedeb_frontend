e8465966748b9e0fd90650d5c71f6891fe854ec9
rebeckaw <rebecka.weegar@umu.se>
2024-04-02 12:49:52 +0200

    metadatabox

--- src/components/ShowData.vue
+++ src/components/ShowData.vue
@@ -18,16 +18,10 @@
             <b>{{ customKey(key) }}:</b>
             {{ value.map((gender_id) => store.options.gender[gender_id]).join(", ") }}
           </div>
-          <div v-else-if="key === 'gender'">
-            <b>{{ customKey(key) }}: </b>Alla
-          </div>
-          <div v-else-if="Array.isArray(value) && value.length > 0">
+          <div v-else-if="key === 'party' && value.length > 0">
             <b>{{ customKey(key) }}:</b> {{ value.join(", ") }}
           </div>
-          <div v-else-if="key==='office' || key === 'subOffice'">
-            <!-- If the key is 'office' or 'subOffice', do nothing for now -->
-          </div>
-          <div v-else-if="value.length == 0">
+          <div v-else-if="(key === 'gender' || key === 'speakers' || key === 'party') && value.length == 0">
             <b>{{ customKey(key) }}:</b> Alla
           </div>
         </div>
