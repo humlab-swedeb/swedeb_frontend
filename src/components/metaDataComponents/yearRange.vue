7f56376325010e7a8df7824197266e3ac129d913
rebeckaw <rebecka.weegar@umu.se>
2024-04-04 15:30:16 +0200

    Showing year range in Anföranden

--- src/components/metaDataComponents/yearRange.vue
+++ src/components/metaDataComponents/yearRange.vue
@@ -2,29 +2,24 @@
   <q-item-label class="text-bold">{{ $t("year") + ":" }}</q-item-label>
   <q-list>
     <q-item class="q-px-none">
-      <q-item-label>{{ min }}</q-item-label>
+      <q-item-label>{{ store.options.yearRange.min }}</q-item-label>
       <q-range
         v-model="store.selected.yearRange"
-        :min="min"
-        :max="max"
+        :min="store.options.yearRange.min"
+        :max="store.options.yearRange.max"
         label-always
         class="q-mt-md q-mb-lg"
         switch-label-side
         color="accent"
       />
-      <q-item-label>{{ max }}</q-item-label>
+      <q-item-label>{{ store.options.yearRange.max }}</q-item-label>
     </q-item>
   </q-list>
 </template>
 
 <script setup>
 import { metaDataStore } from "src/stores/metaDataStore.js";
-import { ref } from "vue";
 
 const store = metaDataStore();
-
-let min = ref(store.options.yearRange.min);
-let max = ref(store.options.yearRange.max);
-
 
 </script>
