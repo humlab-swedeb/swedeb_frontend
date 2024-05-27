85e9fa18ea1d52bd715c987a0cb0918abaf19312
rebeckaw <rebecka.weegar@umu.se>
2024-05-27 16:55:28 +0200

    loading icon shows up sooner

--- src/pages/KWICPage.vue
+++ src/pages/KWICPage.vue
@@ -6,7 +6,9 @@
     <div class="q-pb-md">
       <ShowData />
     </div>
+    <div>hej</div>
     <loadingIcon v-if="loading" size="100" />
+
     <div v-else class="q-pb-xl">
       <div class="row q-py-md justify-between">
         <q-item-label
@@ -39,7 +41,7 @@ import { metaDataStore } from "src/stores/metaDataStore.js";
 import { kwicDataStore } from "src/stores/kwicDataStore";
 import { wordTrendsDataStore } from "src/stores/wordTrendsDataStore";
 import i18n from "src/i18n/sv";
-import { ref, watchEffect } from "vue";
+import { ref, watchEffect, nextTick } from "vue";
 
 const metaStore = metaDataStore();
 const kwicStore = kwicDataStore();
@@ -51,12 +53,16 @@ const formattedIntro = intro;
 const showData = ref(false);
 const loading = ref(false);
 
+
+
 watchEffect(async () => {
   if (metaStore.submitEvent && metaStore.updateEvent) {
+    console.log(' should show wheel')
     loading.value = true;
+    showData.value = true; // Otherwise the loading icon does not show until second search/after pending
+    await nextTick();
     const textString = wtStore.generateStringOfSelected();
     await kwicStore.getKwicResult(textString);
-    showData.value = true;
     setTimeout(() => {
       loading.value = false;
     }, 400);
