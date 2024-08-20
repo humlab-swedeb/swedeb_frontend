045fc9f035faa1755922bd6efda5f4e9c4657db4
kpaalm <kajsa.palm@umu.se>
2024-08-20 13:49:26 +0200

    updated text, contact text, contact btn, title of people, email-link to F

--- src/pages/FAQPage.vue
+++ src/pages/FAQPage.vue
@@ -1,15 +1,18 @@
 <template>
   <q-card flat class="background row justify-center max-width q-mt-lg">
-    <q-item-label class="text-h1 q-px-md">{{ $t("faqPageTitle") }}</q-item-label>
+    <q-item-label class="text-h1 q-px-md">{{
+      $t("faqPageTitle")
+    }}</q-item-label>
     <q-card-section
       class="text-body2 q-mt-sm"
       :class="$q.screen.lt.sm ? '' : 'text-align'"
     >
-      {{ $t("indexPageIntroText") }}
-
-      <div v-for="(faq, index) in faq" :key="index" class="q-pt-md">
+      <div v-for="(faq, index) in faqContent" :key="index" class="q-pt-md">
         <q-item-label class="text-h6 q-mb-sm">{{ faq.q }}</q-item-label>
-        <p class="text-body2">{{ faq.a }}</p>
+        <q-item-label class="text-body2">
+          {{ faq.a }}
+          <a v-if="faq.link" :href="faq.link" class='link-deco text-accent text-bold'>{{ faq.linkText }}</a>
+        </q-item-label>
       </div>
     </q-card-section>
   </q-card>
@@ -18,5 +21,5 @@
 <script setup>
 import i18n from "src/i18n/sv";
 
-const faq = i18n.faqContent;
+const faqContent = i18n.faqContent;
 </script>
