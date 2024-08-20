2fffbaf4861c266ba84ba413781c53063e21ac01
rebeckahw <rebecka.weegar@gmail.com>
2024-08-20 15:17:50 +0200

    Update axios.js

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -8,7 +8,7 @@ import axios from "axios";
 // "export default () => {}" function below (which runs individually
 // for each client)
 
-const api = axios.create({ baseURL: "http://127.0.0.1:5000/v1" });
+const api = axios.create({ baseURL: "https://swedeb.se/v1" });
 
 export default boot(({ app }) => {
   // for use inside Vue files (Options API) through this.$axios and this.$api
