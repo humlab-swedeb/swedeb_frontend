0b8985e6c593fd8d398ca83258f50c1eff6e3b4d
kpaalm <kajsa.palm@umu.se>
2024-09-25 10:35:27 +0200

    update of texts and buttons for reporting feedback.

--- src/components/reportForm.vue
+++ src/components/reportForm.vue
@@ -1,47 +1,55 @@
 <template>
   <q-dialog v-model="dialogVisible" @hide="handleClose">
-    <q-card class="q-px-xl q-py-lg">
-      <q-card-section class="q-px-none q-pb-sm">
-        <p class="text-h6 q-mb-none">
-          Du kommer skickas vidare att lämna din feedback till SweRiks github
-          sida.
-        </p>
+    <q-card class="q-pa-lg bg-primary">
+      <q-card-section class="q-pb-sm">
+        <q-item-label class="text-h6 q-mb-none">
+          {{ $t("reportTitle") }}
+        </q-item-label>
+        <q-item-label>
+          {{ $t("reportText") }}
+        </q-item-label>
       </q-card-section>
       <q-card-section>
         <q-item-label class="text-subtitle1 text-bold q-pb-sm">
-          Metadata
+          {{ $t("meta") }}
         </q-item-label>
-        <div
-          v-for="(value, key) in feedbackStore.getFeedbackVariables(
-            feedbackStore.data
-          )"
-          :key="key"
-        >
-          {{ key }}: {{ value }}
+        <div class="bg-grey-4 q-pa-md q-pr-xl" style="max-width: fit-content">
+          <div
+            class="text-caption text-bold"
+            v-for="(value, key) in feedbackStore.getFeedbackVariables(
+              feedbackStore.data
+            )"
+            :key="key"
+          >
+            {{ key }}: {{ value }}
+          </div>
         </div>
-        <q-btn @click="copy">COPY</q-btn>
-      </q-card-section>
-      <q-card-actions align="center">
         <q-btn
-          flat
-          label="Stäng"
           no-caps
-          @click="handleClose"
-        ></q-btn>
+          flat
+          color="accent"
+          :icon="copyIcon"
+          :label="copyLabel"
+          @click="copy"
+        />
+      </q-card-section>
+      <q-card-actions align="right">
+        <q-btn flat label="Stäng" no-caps @click="handleClose" icon="close" />
         <q-btn
           label="Gå vidare till Github"
           href="https://github.com/orgs/swerik-project/discussions/categories/report-errors"
           target="_blank"
           color="accent"
           no-caps
-        ></q-btn>
+          icon-right="open_in_new"
+        />
       </q-card-actions>
     </q-card>
   </q-dialog>
 </template>
 
 <script setup>
-import { ref, defineProps, watch, defineEmits, watchEffect } from "vue";
+import { ref, defineProps, watch, defineEmits } from "vue";
 import { feedbackDataStore } from "src/stores/feedbackDataStore";
 
 const feedbackStore = feedbackDataStore();
@@ -50,11 +58,15 @@ const props = defineProps({
   clicked: Boolean,
 });
 
+const copyIcon = ref("content_copy");
+const copyLabel = ref("Kopiera metadata");
+
 const copy = () => {
   navigator.clipboard.writeText(
     JSON.stringify(feedbackStore.getFeedbackVariables(feedbackStore.data))
   );
-  alert("Copied");
+  copyIcon.value = "done";
+  copyLabel.value = "Kopierat!";
 };
 
 const dialogVisible = ref(props.clicked);
