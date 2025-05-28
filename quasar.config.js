689fe8a8b7cf0313f1eca580031817585cbfd83e
roger-mahler <roger.mahler@hotmail.com>
2025-05-28 20:21:56 +0200

    change: connfigure axios to use relative baseURL

--- quasar.config.js
+++ quasar.config.js
@@ -46,7 +46,9 @@ module.exports = configure(function (ctx) {
     // Full list of options: https://v2.quasar.dev/quasar-cli-webpack/quasar-config-js#Property%3A-build
     build: {
       vueRouterMode: "hash", // available values: 'hash', 'history'
-
+      env: {
+        API: JSON.stringify('/v1')
+      },
       // transpile: false,
       // publicPath: '/',
 
